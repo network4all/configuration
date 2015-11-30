dcaa3cc53e92b455aca39164b695016717cdacbd
network4all <arjenkuindersma@gmail.com>
2015-11-30 13:43:54 +0100

    First commit..

---
+++ configuration.go
@@ -0,0 +1,37 @@
+package configuration
+
+import (
+    "encoding/json"
+    "os"
+    "fmt"
+)
+
+type Settings struct {
+   GlobalDebug string
+   DBdebug string
+   DBhost string
+   DBname string
+   DBuser string
+   DBpass string
+}
+
+var Gdebug bool
+
+func init() {
+   Gdebug = false
+}
+
+func Loadsettings(conf *Settings) {
+   file, _ := os.Open("conf.json")
+   decoder := json.NewDecoder(file)
+   err := decoder.Decode(&conf)
+   if err != nil {
+      fmt.Println("configuration module: Error in configuration file:", err)
+   } else {
+      if (conf.GlobalDebug == "1") {
+         Gdebug = true
+         fmt.Println("configuration module: config file loaded!")
+      }
+   }
+}
+
